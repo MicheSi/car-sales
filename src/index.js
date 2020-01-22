ee0fd12b9336117ea16a1a6c1ba433dc5264278f
mlazzell17@gmail.com <MicheSi>
2020-01-22 13:19:45 -0700

    created store and moved state to reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import { addedFeatureReducer } from './reducers/AddedFeatureReducer';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(addedFeatureReducer);
+  console.log(store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider>
+    <App />
+</Provider>,
+rootElement);
